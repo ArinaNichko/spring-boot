3ce5a313d5b9a963a6826ac2adc19e39b3f3a90d
ArinaNichko <104902526+ArinaNichko@users.noreply.github.com>
2023-05-23 12:39:19 +0300

    Added configuration files

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,13 +1,25 @@
 package com.example.springboot;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+@Log4j2
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    new SpringApplicationBuilder()
+            .profiles("test")
+            .sources(Application.class)
+            .run(args);
   }
 
+  @Bean
+  ApplicationRunner applicationRunner(Environment environment) {
+    return args -> log.info("message from application.properties " + environment.getProperty("message"));
+  }
 }
